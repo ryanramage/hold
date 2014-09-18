3b8b135212f3ddd4dfce4f05f68cf3c986015a48
Ryan Ramage <ryan.ramage@gmail.com>
2014-09-18 17:29:16 -0600

    spacing

--- lib/BigNumber/BigNumber.cpp
+++ lib/BigNumber/BigNumber.cpp
@@ -1,6 +1,6 @@
 //
 //  BigNumber.cpp
-//  
+//
 //  Author:  Nick Gammon
 //  Date:    22nd January 2013.
 //  Version: 3.1
@@ -35,14 +35,14 @@ BigNumber::BigNumber (const BigNumber & rhs)
   if (this != &rhs)
     num_ = bc_copy_num (rhs.num_);
   }  // end of BigNumber::BigNumber
-  
+
 //operator=
 BigNumber & BigNumber::operator= (const BigNumber & rhs)
 {
   // gracefully handle self-assignment (eg. a = a;)
   if (this == &rhs )
     return *this;
-  
+
   bc_free_num (&num_);  // get rid of old one
   num_ = bc_copy_num (rhs.num_);
   return *this;
@@ -88,7 +88,7 @@ void BigNumber::finish ()
 char * BigNumber::toString () const
 {
   return bc_num2str(num_);
-} // end of BigNumber::toString 
+} // end of BigNumber::toString
 
 BigNumber::operator long () const
 {
@@ -106,54 +106,54 @@ size_t BigNumber::printTo(Print& p) const
 
 // add
 BigNumber & BigNumber::operator+= (const BigNumber & n)
-{ 
+{
   bc_num result = NULL;
   bc_add (num_, n.num_, &result, scale_);
   bc_free_num (&num_);
   num_ = result;
-  return *this; 
-} // end of BigNumber::operator+= 
+  return *this;
+} // end of BigNumber::operator+=
 
 // subtract
 BigNumber & BigNumber::operator-= (const BigNumber & n)
-{ 
+{
   bc_num result = NULL;
   bc_sub (num_, n.num_, &result, scale_);
   bc_free_num (&num_);
   num_ = result;
-  return *this; 
+  return *this;
 }  // end of BigNumber::operator-=
 
 // divide
 BigNumber & BigNumber::operator/= (const BigNumber & n)
-{ 
+{
   bc_num result = NULL;
   bc_init_num (&result);  // in case zero
   bc_divide (num_, n.num_, &result, scale_);
   bc_free_num (&num_);
   num_ = result;
-  return *this; 
-} // end of BigNumber::operator/= 
+  return *this;
+} // end of BigNumber::operator/=
 
 // multiply
 BigNumber & BigNumber::operator*= (const BigNumber & n)
-{ 
+{
   bc_num result = NULL;
   bc_multiply (num_, n.num_, &result, scale_);
   bc_free_num (&num_);
   num_ = result;
-  return *this; 
+  return *this;
 }  // end of BigNumber::operator*=
 
 // modulo
 BigNumber & BigNumber::operator%= (const BigNumber & n)
-{ 
+{
   bc_num result = NULL;
   bc_init_num (&result);  // in case zero
   bc_modulo (num_, n.num_, &result, scale_);
   bc_free_num (&num_);
   num_ = result;
-  return *this; 
+  return *this;
 }  // end of BigNumber::operator%=
 
 
